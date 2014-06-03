6c6175daec8edfc3fe13ff2e14583ffa047cf839
boboman13 <rich@safe-node.com>
2014-06-02 21:05:57 -0400

    Adds closure for issue of routing

--- src/staticize.js
+++ src/staticize.js
@@ -5,18 +5,20 @@ module.exports = function(app, routes) {
 	// loop through routes
 	for (var index in routes) {
 		// route: index, file location: routes[index]
-		app.get(index, function(req, res) {
-			fs.readFile(routes[index], function(err, data) {
-				if (err) {
-					console.log('ERROR: Could not read file (' + err + ').');
-					res.status(500);
-					return;
-				}
+		(function (indexRoute) {
+			app.get(index, function(req, res) {
+				fs.readFile(routes[index], function(err, data) {
+					if (err) {
+						console.log('ERROR: Could not read file (' + err + ').');
+						res.status(500);
+						return;
+					}
 
-				// get the mime type
-				var mimeType = mime.lookup(routes[index]);
-				res.status(200).set('Content-Type', mimeType).send(data);
+					// get the mime type
+					var mimeType = mime.lookup(routes[index]);
+					res.status(200).set('Content-Type', mimeType).send(data);
+				});
 			});
-		});
+		})(index);
 	}
 };
