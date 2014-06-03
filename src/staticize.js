bb508dc49c2012ccaf1d6aebcd82c16336ce1d82
boboman13 <rich@safe-node.com>
2014-06-02 21:14:24 -0400

    Fixes issue

--- src/staticize.js
+++ src/staticize.js
@@ -6,8 +6,8 @@ module.exports = function(app, routes) {
 	for (var index in routes) {
 		// route: index, file location: routes[index]
 		(function (indexRoute) {
-			app.get(index, function(req, res) {
-				fs.readFile(routes[index], function(err, data) {
+			app.get(indexRoute, function(req, res) {
+				fs.readFile(routes[indexRoute], function(err, data) {
 					if (err) {
 						console.log('ERROR: Could not read file (' + err + ').');
 						res.status(500);
@@ -15,7 +15,7 @@ module.exports = function(app, routes) {
 					}
 
 					// get the mime type
-					var mimeType = mime.lookup(routes[index]);
+					var mimeType = mime.lookup(routes[indexRoute]);
 					res.status(200).set('Content-Type', mimeType).send(data);
 				});
 			});
